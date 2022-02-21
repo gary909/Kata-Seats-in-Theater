08c88785092da9ef3370c53456144297ff8f841b
gary <gary.white@lodgit.com>
2022-02-21 13:32:57 +0100

    code

---
+++ app.js
@@ -0,0 +1,10 @@
+function seatsInTheater(nCols, nRows, col, row) {
+    //coding and coding..
+    
+    
+}
+
+console.log(seatsInTheater(16,11,5,3)); // 96
+console.log(seatsInTheater(1,1,1,1)); // 0
+console.log(seatsInTheater(13,6,8,3)); // 18
+console.log(seatsInTheater(60,100,60,1)); // 99
